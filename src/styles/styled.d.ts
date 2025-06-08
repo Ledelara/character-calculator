69ddc36fef5fa11b204db9dec2a9a18813816ee1
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-06-08 15:32:39 -0300

    🚧 feat: tema dinâmico

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,12 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    bodyBg: string;
+    textColor: string;
+    paragraphColor: string;
+    headerBg: string;
+    typographyBorder: string;
+    letterDistance: string;
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+export const lightTheme = {
+  bodyBg: "var(--neutral-0)",
+  textColor: "var(--neutral-800)",
+  paragraphColor: "var(--neutral-800)",
+  headerBg: "var(--neutral-0)",
+  typographyBorder: "var(--neutral-200)",
+
+  letterDistance: "var(--purple-400)",
+};
+
+export const darkTheme = {
+  bodyBg: "var(--neutral-600)",
+  textColor: "var(--neutral-0)",
+  paragraphColor: "var(--neutral-0)",
+  headerBg: "var(--neutral-600)",
+  typographyBorder: "var(--neutral-200)",
+
+  letterDistance: "var(--purple-500)",
+};
+
+
+export type ThemeType = typeof lightTheme;
