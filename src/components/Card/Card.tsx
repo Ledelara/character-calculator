2e327b10b64b6ba95861c200f40d75d79f15572a
Fernando Azevedo <fsazevedo09@gmail.com>
2025-06-10 20:12:03 -0300

    💄 style: estilizando componente Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,11 +2,26 @@ import { Card } from "../StyledComponents/Card/Card";
 
 export function CardComponent() {
     return (
-        <Card>
-            <div className="card">
-                <h2>00</h2>
-                <p>Test</p>
-            </div>
-        </Card>
+        <>
+            <Card>
+                <div className="card total-characters-card">
+                    <span className="card-count">00</span>
+                    <p>Total Characters</p>
+                </div>
+            </Card>
+            <Card>
+                <div className="card word-characters-card">
+                    <span className="card-count">00</span>
+                    <p>Word Count</p>
+                </div>
+            </Card>
+            <Card>
+                <div className="card sentence-characters-card">
+                    <span className="card-count">00</span>
+                    <p>Sentence Count</p>
+                </div>
+            </Card>
+
+        </>
     )
 }
